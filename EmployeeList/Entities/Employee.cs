c517d2ee1ca0bdf69ac1b784668c4896fcf5f45a
Alison Demian Kemle <alisondemian@gmail.com>
2020-03-19 17:50:43 -0300

    EmployeeList em andamento

---
+++ EmployeeList/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeList.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+
+    }
+}
---
+++ EmployeeList/Entities/OutsourceEployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeList.Entities
+{
+    class OutsourceEployee : Employee
+    {
+        public double AdditionalCharge;
+
+
+        public override double Payment()
+        {
+            
+            return base.Payment() + (AdditionalCharge + (AdditionalCharge*0.1)); 
+
+        }
+
+
+    }
+}
